62f2fee783463f68e26397a46f37cf85488dd53c
Said Tahsin Dane <tasomaniac@gmail.com>
2015-07-18 18:09:13 +0300

    Status URL is updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module("GarajApp", ['ngtimeago']);
 app.controller('garajCtrl', function($scope, $http){
-	$http.get("http://spaceapi.net/cache/Garaj")
+	$http.get("http://api.garaj.co/spaceapi")
 		.success(function(response){
 			$scope.liste = response;
 			$scope.lastChangeDate = new Date(response.state.lastchange * 1000);
