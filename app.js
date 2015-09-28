6d6ae9793fd987dfc254501aa72a0006b15298ee
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2015-09-28 11:34:39 +0300

    use ngCloak while loading web page use local files such as js and font

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module("GarajApp", ['ngtimeago']);
 app.controller('garajCtrl', function($scope, $http){
 	$http.get("http://api.garaj.co/spaceapi")
 		.success(function(response){
-			$scope.liste = response;
+			$scope.garaj = response;
 			$scope.lastChangeDate = new Date(response.state.lastchange * 1000);
 		});
-})
+});
